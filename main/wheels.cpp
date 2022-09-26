d26c4ecbe06fcf89abcec18da829e1158f68a9d0
Duncan Britt <dbru997@gmail.com>
2022-09-26 09:13:38 -0600

    Moved Wheels class to separate files.

---
+++ main/wheels.cpp
@@ -0,0 +1,58 @@
+#include "wheels.h"
+
+void Wheels::go() {
+  Serial.println("Going!");
+  digitalWrite(pin_left_bw, LOW);
+  digitalWrite(pin_right_bw, LOW);
+  digitalWrite(pin_left_fw, HIGH);
+  digitalWrite(pin_right_fw, HIGH);
+  analogWrite(pin_motor_speed, speed);
+}
+
+void Wheels::stop() {
+  Serial.println("Stopped.");
+  digitalWrite(pin_left_bw, LOW);
+  digitalWrite(pin_right_bw, LOW);
+  digitalWrite(pin_left_fw, LOW);
+  digitalWrite(pin_right_fw, LOW);
+  analogWrite(pin_motor_speed, 0);
+}
+
+void Wheels::reverse() {
+  Serial.println("Backing up.");
+  digitalWrite(pin_left_fw, LOW);
+  digitalWrite(pin_right_fw, LOW);
+  digitalWrite(pin_left_bw, HIGH);
+  digitalWrite(pin_right_bw, HIGH);
+  analogWrite(pin_motor_speed, speed);
+}
+
+void Wheels::turn_right() {
+  Serial.println("Right Turn");
+  stop();
+  digitalWrite(pin_right_bw, HIGH);
+  digitalWrite(pin_left_fw, HIGH);
+  analogWrite(pin_motor_speed, SLOW);
+}
+
+void Wheels::turn_left() {
+  Serial.println("Left Turn");
+  stop();
+  digitalWrite(pin_left_bw, HIGH);
+  digitalWrite(pin_right_fw, HIGH);
+  analogWrite(pin_motor_speed, SLOW);
+}
+
+void Wheels::set_speed(byte new_speed) {
+  if (speed < 0 || speed > 255) { // Invalid speed
+    return;
+  }
+
+  if (speed == 0) { // If not currently moving.
+    speed = new_speed;
+    return;
+  }
+
+  analogWrite(pin_motor_speed, speed);
+  speed = new_speed;
+}
---
+++ main/wheels.h
@@ -0,0 +1,35 @@
+#ifndef GUARD_wheels_h
+#define GUARD_wheels_h
+
+#include <Arduino.h>
+
+class Wheels {
+public:
+  // Sets motor control pins for the wheels to OUTPUT, initializes members which refer to wheel motor control pins. 
+  Wheels(const byte left_forward, const byte left_backward, const byte right_forward, const byte right_backward, const byte motor_speed) : 
+         pin_left_fw(left_forward), pin_left_bw(left_backward), pin_right_fw(right_forward), pin_right_bw(right_backward), pin_motor_speed(motor_speed) {
+    pinMode(left_forward, OUTPUT);
+    pinMode(left_backward, OUTPUT);
+    pinMode(right_forward, OUTPUT);
+    pinMode(right_backward, OUTPUT);
+    pinMode(motor_speed, OUTPUT);
+  }
+  void go();
+  void stop(); // Need to investigate breaking. Currently just stops voltage to the wheels.
+  void reverse();
+  void turn_right(); // Tank turns:
+  void turn_left();  // One side spins forward, the other backward.
+  void set_speed(byte new_speed);
+  static const byte FAST = 255;   // Example use: 
+  static const byte MEDIUM = 180; // wheels.set_speed(Wheels::MEDIUM); 
+  static const byte SLOW = 120;   //
+private:
+  byte speed = MEDIUM; // Default speed
+  byte pin_left_fw;
+  byte pin_left_bw;
+  byte pin_right_fw;
+  byte pin_right_bw;
+  byte pin_motor_speed;
+};
+
+#endif
