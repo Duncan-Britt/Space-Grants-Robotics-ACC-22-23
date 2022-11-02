06866b5cac21da1886a86a29f6272e1de4b078dc
Duncan Britt <dbru997@gmail.com>
2022-11-02 16:26:56 -0600

    Added library for IR sensor

--- main/main.ino
+++ main/main.ino
@@ -1,34 +1,51 @@
 #include "wheels.h"
+#include <SharpIR.h>
 
-const int ledPin = 2; // temporary for testing
-
-void setup() {
-  Serial.begin(9600); // Needed to print to Serial Monitor.
-  pinMode(ledPin, OUTPUT);    // { temporary
-  digitalWrite(ledPin, HIGH); //   for testing }
-  Serial.println("\nSetup complete.\n");
-}
-
-// Need to figure out what the actual pins will be.
-#define PIN_LEFT_FW 2
-#define PIN_LEFT_BW 4
-#define PIN_RIGHT_FW 7
-#define PIN_RIGHT_BW 8
+#define PIN_LEFT_FW 8
+#define PIN_LEFT_BW 9
+#define PIN_RIGHT_FW 10
+#define PIN_RIGHT_BW 11
 #define PIN_WHEELS_SPEED 3
 #define PIN_WHEELS_SPEED2 5
 
+void setup() {
+  Serial.begin(9600); // Needed to print to Serial Monitor.
+
+  pinMode(PIN_LEFT_FW, OUTPUT);
+  pinMode(PIN_LEFT_BW, OUTPUT);
+  pinMode(PIN_RIGHT_FW, OUTPUT);
+  pinMode(PIN_RIGHT_BW, OUTPUT);
+  pinMode(PIN_WHEELS_SPEED, OUTPUT);
+  pinMode(PIN_WHEELS_SPEED2, OUTPUT);
+}
+
 void loop() {
-  static const Wheels wheels(PIN_LEFT_FW, PIN_LEFT_BW, PIN_RIGHT_FW, PIN_RIGHT_BW, PIN_WHEELS_SPEED, PIN_WHEELS_SPEED2);
-  // Test script {
-  wheels.go();
+  digitalWrite(PIN_LEFT_BW, LOW);
+  digitalWrite(PIN_RIGHT_BW, LOW);
+  digitalWrite(PIN_LEFT_FW, HIGH);
+  digitalWrite(PIN_RIGHT_FW, HIGH);
+  analogWrite(PIN_WHEELS_SPEED, 255);
+  analogWrite(PIN_WHEELS_SPEED2, 255);
   delay(3000);
-  wheels.reverse();
-  delay(3000);
-  wheels.turn_right();
-  delay(3000);
-  wheels.turn_left();
-  delay(3000);
-  wheels.stop();
-  delay(5000);
+
+  // static const Wheels wheels(PIN_LEFT_FW, PIN_LEFT_BW, PIN_RIGHT_FW, PIN_RIGHT_BW, PIN_WHEELS_SPEED, PIN_WHEELS_SPEED2);
+  // // Test script {
+  // wheels.go();
+  // delay(3000);
+  // wheels.reverse();
+  // delay(3000);
+  // wheels.turn_right();
+  // delay(3000);
+  // wheels.turn_left();
+  // delay(3000);
+  // wheels.stop();
+  // delay(5000);
   // }
 }
+
+// pins 10 & 11 are connected to in 3 & in 4 which should control the right drive?
+// 10 is right forward
+// 11 is right backward
+
+// 8 is left forward
+// 9 is left backward
