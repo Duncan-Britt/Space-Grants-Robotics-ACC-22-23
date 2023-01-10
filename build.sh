3609dac663cbe8c9eb4f82a39dfc68911568d2cf
Duncan Britt <dbru997@gmail.com>
2023-01-10 00:19:40 -0700

    Updated build.sh to reflect name changes

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -xe
 
-g++ -x c++ -fsanitize=address -std=c++11 -Wall -Wextra main/main.ino main/astar.cpp main/motors.cpp main/pose.cpp main/SharpIR.cpp main/debug.cpp -o app
+g++ -x c++ -fsanitize=address -std=c++11 -Wall -Wextra main/main.ino main/grid.cpp main/motors.cpp main/pose.cpp main/SharpIR.cpp main/debug.cpp -o app
