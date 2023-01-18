5497f6ccca202e78aa32758053fc20727a016486
insertgamertaghere1 <114264576+insertgamertaghere1@users.noreply.github.com>
2023-01-18 16:41:33 -0700

    Add files via upload
    combined 9-dof stuff i have so far, struggling a little hoping to get some help on friday

---
+++ pitchrollheading/9DOF_Code.ino
@@ -0,0 +1,86 @@
+//Unified System
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+  
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+//Unified System
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println("Orientation Sensor Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+  delay(1000);
+    
+  bno.setExtCrystalUse(true);
+}
+
+
+//unified system
+void loop(void) 
+{
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  bno.getEvent(&event);
+  
+  /* Display the floating point data */
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  Serial.print("\tY: ");
+  Serial.print(event.orientation.y, 4);
+  Serial.print("\tZ: ");
+  Serial.print(event.orientation.z, 4);
+  Serial.println("");
+  
+  delay(100);
+}
+
+//data viewing I think
+  // Possible vector values can be:
+  // - VECTOR_ACCELEROMETER - m/s^2
+  // - VECTOR_MAGNETOMETER  - uT
+  // - VECTOR_GYROSCOPE     - rad/s
+  // - VECTOR_EULER         - degrees
+  // - VECTOR_LINEARACCEL   - m/s^2
+  // - VECTOR_GRAVITY       - m/s^2
+  //imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  
+  /* Display the floating point data */
+  /*Serial.print("X: ");
+  Serial.print(euler.x());
+  Serial.print(" Y: ");
+  Serial.print(euler.y());
+  Serial.print(" Z: ");
+  Serial.print(euler.z());
+  Serial.println("");*/
+
+  /* Display the current temperature */
+//int8_t temp = bno.getTemp();
+
+/*Serial.print("Current Temperature: ");
+Serial.print(temp);
+Serial.println(" C");
+Serial.println("");*/
+
+//imu::Quaternion quat = bno.getQuat();
+
+/* Display the quat data */
+/*Serial.print("qW: ");
+Serial.print(quat.w(), 4);
+Serial.print(" qX: ");
+Serial.print(quat.y(), 4);
+Serial.print(" qY: ");
+Serial.print(quat.x(), 4);
+Serial.print(" qZ: ");
+Serial.print(quat.z(), 4);
+Serial.println("");*/
